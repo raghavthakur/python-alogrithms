aafa290e7cc3e375b61c92d60a6fe4ec84a07d73
Raghav Thakur <raghav.thakur.rt.20@gmail.com>
2020-06-16 18:30:23 -0700

    added selection_sort.py

---
+++ selection_sort.py
@@ -0,0 +1,19 @@
+# find the smallest element in the given array
+def findSmallest(arr):
+    smallest = arr[0]
+    smallest_index = 0
+    for i in range(1, len(arr)):
+        if arr[i] < smallest:
+            smallest = arr[i] # set new smallest
+            smallest_index = i
+    return smallest_index
+
+# sort the given array in increasing order
+def selectionSort(arr):
+    new_arr = []
+    for i in range(len(arr)):
+        smallest = findSmallest(arr) # finds the smallest element in the array
+        new_arr.append(arr.pop(smallest)) # and add it to new array
+    return new_arr
+
+print(selectionSort([5, 3, 6, 2, 10]))
