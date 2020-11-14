12451a5de622cfbd028476964c6114d694fb015b
Raghav Thakur <raghav.thakur.rt.20@gmail.com>
2020-11-13 20:33:15 -0800

    Modified quicksort.py

--- quicksort.py
+++ quicksort.py
@@ -1,1 +1,5 @@
 # Quicksort uses divide-and-conquer (recursive technique for solving problems)
+# Divide and Conquer Steps:
+# 1. Figure out base case. This should be simplest case.
+# 2. Divide or decrease your problem until it becomes the base case.
+
